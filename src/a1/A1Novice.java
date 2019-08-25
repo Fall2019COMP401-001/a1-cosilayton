f477451add490243996298766bf7517b5bd95f0b
cosilayton <cosilayton@172.17.98.188>
2019-08-25 16:31:12 -0400

    Entered code for novice level.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -5,10 +5,47 @@ import java.util.Scanner;
 public class A1Novice {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		// read in number of customers
+		int count = scan.nextInt();
+		String[] names = new String[count];
+		double[] prices = new double[count];
 		
+		// get input
+		for (int j = 0; j < count; j++) {
+
+			// read in full name of customer
+			String firstName = scan.next();
+			String lastName = scan.next();
+
+			// read in total number of different items
+			int items = scan.nextInt();
+
+			// information on each type of item
+			double customerSum = 0;
+			for (int i = 0; i < items; i++) {
+				int quantity = scan.nextInt();
+				scan.next(); // reads the next word without saving it
+				double price = scan.nextDouble();
+
+				double totalPrice = quantity * price;
+				customerSum = customerSum + totalPrice;
+			}
+			// compiling the names to print and total sum spent 
+			String nameToPrint = firstName.charAt(0) + ". " + lastName;
+			names[j] = nameToPrint;
+			prices[j] = customerSum;
+			
+			
+
+		}
+		
+		// print output
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i] + ": " + prices[i]); 
+		}
+
 	}
 }
